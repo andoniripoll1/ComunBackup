ab3ebbce0cea73cec598a90c6348a709e84e9288
gruposogecable\aripollj <andoni.ripolljarauta.ext@telefonica.com>
2020-10-29 17:33:05 +0100

    Backup

---
+++ AndoIt.Common.Test/Common/Integration/GoodConfigOnDaoTest.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using AndoIt.Common.Interface;
+
+namespace AndoIt.Common.Test.Common.Integration
+{
+	[TestClass]
+	public class GoodConfigOnDaoTest
+	{   
+        public GoodConfigOnDaoTest()
+		{
+			Mock<IIoCObjectContainer> inizializeIoCObjectContainer = new TestsHelper().InizializeIoCObjectContainer();
+		}
+
+		[TestMethod]
+		public void GetJNodeByTagAddress_Normal_StringGiven()
+		{
+			//	Arrange
+			Mock<IIoCObjectContainer> inizializeIoCObjectContainer = new TestsHelper().InizializeIoCObjectContainer();
+			GoodConfigOnDao toTest = new GoodConfigOnDao(inizializeIoCObjectContainer.Object, CONNECTIONSTRING, "SoloParaTest");
+
+			//	Act
+
+			//	Assert
+			Assert.AreEqual(toTest.GetJNodeByTagAddress("Deployment").ToString(), "TEST");
+		}
+		[TestMethod]
+		public void GetJNodeByTagAddress_Normal_ComplexObjectGiven()
+		{
+			//	Arrange
+			Mock<IIoCObjectContainer> inizializeIoCObjectContainer = new TestsHelper().InizializeIoCObjectContainer();
+			GoodConfigOnDao toTest = new GoodConfigOnDao(inizializeIoCObjectContainer.Object, CONNECTIONSTRING, "SoloParaTest");
+
+			//	Act
+
+			//	Assert
+			Assert.AreEqual(toTest.GetJNodeByTagAddress("complex").Value<string>("attribute"), "attributeContent");
+		}
+	}
+}
---
+++ Interface/ILog.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace AndoIt.Common.Interface
+{
+	public interface ILog
+	{
+		void Fatal(string message, Exception exception = null, StackTrace stackTrace = null, params object[] paramValues);
+		void Error(string message, Exception exception = null, StackTrace stackTrace = null, params object[] paramValues);
+		void Warn(string message, Exception exception = null, StackTrace stackTrace = null);			
+		void Info(string message, StackTrace stackTrace = null);
+		void InfoSafe(string message, StackTrace stackTrace);
+		void Debug(string message, StackTrace stackTrace = null);        
+    }
+}
